60d179517473b1c2a5ef69f8dd5faa9c9a491fbc
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-02 18:28:24 +0900

    feat: 참가자 명단 입력 기능 구현

--- src/main/java/racingcar/controller/Application.java
+++ src/main/java/racingcar/controller/Application.java
@@ -1,9 +1,13 @@
 package racingcar.controller;
 
 import java.util.Scanner;
+import java.util.List;
+import racingcar.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        final List<String> participants = InputView.getParticipants(scanner);
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,2 +1,13 @@
-package racingcar.view;public class Inputview {
+package racingcar.view;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.Arrays;
+
+public class InputView {
+    public static List<String> getParticipants(final Scanner scanner) {
+        OutputView.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<String> participants = Arrays.asList(scanner.nextLine().split(","));
+        return participants;
+    }
 }
