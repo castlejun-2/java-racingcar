c06950f337938573a1db54bd08cdb541810f625b
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-02 18:55:39 +0900

    feat: 예외 처리 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.Arrays;
 
+import racingcar.domain.Validator;
+import java.lang.IllegalArgumentException;
+
 public class InputView {
     public static List<String> getParticipants(final Scanner scanner) {
-        OutputView.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        List<String> participants = Arrays.asList(scanner.nextLine().split(","));
-        return participants;
+        try {
+            OutputView.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            List<String> participants = Arrays.asList(scanner.nextLine().split(","));
+            Validator.isValidNameList(participants);
+            return participants;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getParticipants(scanner);
+        }
     }
 }
