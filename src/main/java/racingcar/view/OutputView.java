11740c1a0d521b577f2ba4aa881080e11e297dca
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-02 18:54:39 +0900

    feat: 예외 출력 기능 추가

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     final static void println(final String msg) {
         System.out.println(msg);
     }
+
+    final static void printError(final String errorMsg) {
+        System.out.println("[ERROR] "+errorMsg);
+    }
 }
