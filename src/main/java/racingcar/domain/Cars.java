dae0ab6c85cb793664db4bc309abcf2c843bf963
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-03 23:50:40 +0900

    feat: Cars의 instance인 Car 객체 생성 및 초기화 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -4,7 +4,14 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Cars {
-    List<Car> carList = new ArrayList<Car>();
+    List<Car> carList = new ArrayList<>();
 
-    public Cars(final List<String> carNames) {}
+    public Cars(final List<String> carNames) {
+        this.createCars(carNames);
+    }
+
+    public void createCars(List<String> carNames) {
+        for (int i = 0; i < carNames.size(); i++)
+            carList.add(new Car(carNames.get(i)));
+    }
 }
