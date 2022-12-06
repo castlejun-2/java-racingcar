58880417faadd0463a27d0ad3b9850f2822a6f56
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-06 13:32:50 +0900

    test: Car 객체의 현재 위치를 앞으로 1칸 이동하는 함수 테스트

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.controller.Application;
 import racingcar.domain.Car;
@@ -10,8 +12,16 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+    Car testCar;
     @Test
+    @BeforeEach
     void createCarObj() {
-        Car testCar = new Car("test");
+        testCar = new Car("test");
+    }
+
+    @Test
+    @DisplayName("차를 앞으로 한칸 이동 시킨다")
+    void increasePositionTest() {
+        assertThat(testCar.increasePosition(testCar.getPosition())).isEqualTo(1);
     }
 }
