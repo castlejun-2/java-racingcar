1e23fd5b8085dc7613a00a4aa4b4d12ee3396c2f
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-06 12:33:57 +0900

    test: 라운드 감소 함수 테스트

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Cars;
 import racingcar.domain.RacingGame;
@@ -13,11 +15,19 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RacingGameTest {
+    RacingGame racingGame;
     @Test
+    @BeforeEach
     void createRacingGameObj() {
         List<String> names = Arrays.asList("hip,pop,dance");
         Cars cars = new Cars(names);
 
-        RacingGame racingGame = new RacingGame(cars,3);
+        racingGame = new RacingGame(cars,3);
+    }
+
+    @Test
+    @DisplayName("라운드 감소 함수 테스트")
+    void subRoundTest() {
+        assertThat(racingGame.subRound(racingGame.getRound())).isEqualTo(2);
     }
 }
